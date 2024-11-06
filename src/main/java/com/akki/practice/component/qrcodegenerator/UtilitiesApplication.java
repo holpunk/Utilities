6e8ed415d37d744460635a6b0cc80b24de04e1b4
Mr. Ak <aqshey@gmail.com>
2024-11-06 16:04:09 +0100

    reformatted to the utilities

--- src/main/java/com/akki/practice/component/qrcodegenerator/UtilitiesApplication.java
+++ src/main/java/com/akki/practice/component/qrcodegenerator/UtilitiesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QrcodegeneratorApplication {
+public class UtilitiesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(QrcodegeneratorApplication.class, args);
+        SpringApplication.run(UtilitiesApplication.class, args);
     }
 
 }
